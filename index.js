955443d545ee190c630ee6e25b563bb672d850fd
Joe Park-Kennaby <jparkkennaby@gmail.com>
2019-10-03 21:01:25 +0100

    7.5 - setup mongodb schema using mongoose Schema method

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')
 .then( () => console.log('Connected to MongoDB...'))
 .catch(err => console.error('Could not connect to MongoDB', err))
+
+// to connect to mongoDB, make sure mongodb is running on localhost. run:
+// mongod
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+})
