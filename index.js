92e11b33aaf9b29f73efbd86498bf8fcc33732e4
Joe Park-Kennaby <jparkkennaby@gmail.com>
2019-10-05 09:54:49 +0100

    7.8 - query objects from mongodb

--- index.js
+++ index.js
@@ -32,4 +32,23 @@ async function createCourse() {
 
 };
 
-createCourse();
+// createCourse();
+
+// basic mongodb query
+async function getCourses() {
+
+    // const courses = await Course.find(); // return all course objects
+
+    const courses = await Course.
+    find({
+        author: "Mosh",
+        isPublished: true
+    })
+    .limit(10)
+    .sort({ name: 1 }) // means ascending order (-1 = decending)
+    .select({ name: 1, tags: 1 }); // only return name and tags properties
+    console.log(courses);
+
+};
+
+// getCourses();
